44b6dbd37e779fc4b811a7047cd6df5378944fd7
Jose Gallego-Posada <8620503+gallego-posada@users.noreply.github.com>
2025-03-31 16:15:36 -0500

    Example commands

---
+++ cmds.sh
@@ -0,0 +1,9 @@
+uv run clt_at_0.py --sample_size 100 --keep_open 
+
+uv run random_ecdf.py --num_frames 5 --keep_open   
+
+uv run bridge.py --keep_open --num_frames 5 --sample_size 1000
+
+uv run ks_test.py --sample_size 10
+uv run ks_test.py --sample_size 10
+uv run ks_test.py --sample_size 100
